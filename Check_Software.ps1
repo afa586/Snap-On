ebb6efbd9f596b1b85df8c41fde8759ce687b2df
afa586 <40829527+afa586@users.noreply.github.com>
2022-03-24 17:19:27 +0800

    Add files via upload

---
+++ Check_Software.ps1
@@ -0,0 +1,20 @@
+﻿<#--------------------------------------------------------------------------------------------------------------
+Description: The script is used to check software list which installed on the computer
+Author: Tom
+Date: 2018-12-11
+----------------------------------------------------------------------------------------------------------------#>
+#Define
+$reportname=$env:computername+'-'+$env:username
+$report=(Split-Path -Parent $MyInvocation.MyCommand.Definition)+"\reports\$reportname.csv"
+
+#Check software installed
+
+
+$InstalledSoftware = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty)
+$InstalledSoftware += (Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty)
+
+IF (Test-path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) 
+{$InstalledSoftware += (Get-ChildItem HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty) }
+$InstalledSoftware | Where {$_.DisplayName -ne $Null -AND $_.SystemComponent -ne "1" -AND $_.ParentKeyName -eq $Null}|
+Sort-Object -Property DisplayName -Unique |Select-Object DisplayName,DisplayVersion,InstallDate,@{n='Computername';e={$env:computername}} |
+Export-Csv -Path $report -Encoding UTF8 -NoTypeInformation
---
+++ Start-Bitlocker.ps1
@@ -0,0 +1,50 @@
+﻿<#--------------------------------------------------------------------------------------------------------------------
+Author: Tom
+Date:2019-04-16
+Description:The script is used start bitlocker
+-------------------------------------------------------------------------------------------------------------------#>
+#Define
+$x = Split-Path -Parent $MyInvocation.MyCommand.Definition #get current path
+$Log = "$x\Logs\" + $env:computername + ".txt" #set log file address
+$PSLog = "$x\Logs\" + $env:computername + "-PS.txt" #set PS log file address
+try { Start-Transcript -path $PSLog } catch {}
+$EncryptLog = "$x\Logs\EncryptLog.csv" #set encrypted computer log file address
+$SecureString = ConvertTo-SecureString "Snapon@2019" -AsPlainText -Force #Define password
+$partitions=@('D:') #Define the drive need to encrypt, C drive will be encrypted automatically by default
+$TPM = (Get-Tpm).TpmPresent #Check if TPM exist
+
+$ProtectionStatus = (Get-BitLockerVolume -MountPoint C:).ProtectionStatus
+$VolumeStatus = (Get-BitLockerVolume -MountPoint C:).VolumeStatus
+if ($ProtectionStatus -eq "on" -or $VolumeStatus -eq "EncryptionInProgress")
+    {New-Object psobject -Property @{ComputerName = $env:computername;Partition = "C:";Status = $VolumeStatus;Time = Get-Date} |select * |Export-Csv -Path $EncryptLog -Append -NoTypeInformation}
+else
+    {
+    Add-BitLockerKeyProtector -MountPoint "C:" -RecoveryPasswordProtector |Out-File $Log
+    if (!$TPM)
+        {Add-BitLockerKeyProtector -MountPoint "C:" -PasswordProtector -Password $SecureString |Out-File $Log -Append}
+    else {manage-bde -protectors -add C: -TPM |Out-File $Log -Append}
+    manage-bde.exe -on C: -s -used |Out-File $Log -Append
+    }
+
+foreach ($partition in $partitions)
+{
+    if (Test-Path $partition)
+        {
+        $ProtectionStatus = (Get-BitLockerVolume -MountPoint $partition).ProtectionStatus
+        $VolumeStatus = (Get-BitLockerVolume -MountPoint $partition).VolumeStatus
+       if ($ProtectionStatus -eq "on" -or $VolumeStatus -eq "EncryptionInProgress")
+            {New-Object psobject -Property @{ComputerName = $env:computername;Partition = $partition;Status = $VolumeStatus;Time = Get-Date} |select * |Export-Csv -Path $EncryptLog -Append -NoTypeInformation}
+        else
+            {
+            Add-BitLockerKeyProtector -MountPoint $partition -RecoveryPasswordProtector |Out-File $Log -Append
+            Add-BitLockerKeyProtector -MountPoint $partition -PasswordProtector -Password $SecureString |Out-File $Log -Append
+            manage-bde.exe -on $partition -used |Out-File $Log -Append
+            manage-bde -autounlock -enable $partition |Out-File $Log -Append
+            }
+        }
+}
+try { Stop-Transcript } catch {}
+
+
+
+ 
